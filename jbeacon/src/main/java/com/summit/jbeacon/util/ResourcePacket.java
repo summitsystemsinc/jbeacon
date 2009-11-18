6c9e0780261402252ecbfcb4663ecddd4db9252d
Steven Swor <steveswor@gmail.com>
2009-11-18 21:47:56 +0000

    Making ResourcePacket implement Comparable

--- jbeacon/src/main/java/com/summit/jbeacon/util/ResourcePacket.java
+++ jbeacon/src/main/java/com/summit/jbeacon/util/ResourcePacket.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.summit.jbeacon.util;
 
 import java.io.Serializable;
@@ -13,41 +12,46 @@ import java.util.List;
  *
  * @author justin
  */
-public class ResourcePacket implements Serializable{
-    private String defaultHostName;
+public class ResourcePacket implements Serializable, Comparable<ResourcePacket> {
 
-    private List<Resource> resources;
+	private String defaultHostName;
+	private List<Resource> resources;
 
-    public ResourcePacket(){
-        //TODO make this a sorted set
-        resources = new ArrayList();
-    }
+	public ResourcePacket() {
+		//TODO make this a sorted set
+		resources = new ArrayList();
+	}
 
-    /**
-     * @return the resources
-     */
-    public List<Resource> getResources() {
-        return resources;
-    }
+	/**
+	 * @return the resources
+	 */
+	public List<Resource> getResources() {
+		return resources;
+	}
 
-    /**
-     * @param resources the resources to set
-     */
-    public void setResources(List<Resource> resources) {
-        this.resources = resources;
-    }
+	/**
+	 * @param resources the resources to set
+	 */
+	public void setResources(List<Resource> resources) {
+		this.resources = resources;
+	}
 
-    /**
-     * @return the defaultHostName
-     */
-    public String getDefaultHostName() {
-        return defaultHostName;
-    }
+	/**
+	 * @return the defaultHostName
+	 */
+	public String getDefaultHostName() {
+		return defaultHostName;
+	}
 
-    /**
-     * @param defaultHostName the defaultHostName to set
-     */
-    public void setDefaultHostName(String defaultHostName) {
-        this.defaultHostName = defaultHostName;
-    }
+	/**
+	 * @param defaultHostName the defaultHostName to set
+	 */
+	public void setDefaultHostName(String defaultHostName) {
+		this.defaultHostName = defaultHostName;
+	}
+
+	@Override
+	public int compareTo(ResourcePacket o) {
+		return getDefaultHostName().compareToIgnoreCase(o.getDefaultHostName());
+	}
 }
