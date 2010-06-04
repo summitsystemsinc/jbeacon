397ea76ef3e1bc9e41a4c285289ee0646457c262
Justin Smith <magno32@gmail.com>
2010-06-04 15:30:11 +0000

    [maven-release-plugin]  copy for tag jbeacon-0.2.3

--- jbeacon/src/main/java/com/summit/jbeacon/util/NetUtilities.java
+++ jbeacon/src/main/java/com/summit/jbeacon/util/NetUtilities.java
@@ -1,6 +1,17 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2009 Summit Management Systems, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.summit.jbeacon.util;
 
@@ -19,56 +30,56 @@ import java.util.List;
 public class NetUtilities {
 
     public static final List<String> KNOWN_LOCAL_HOSTS =
-            Arrays.asList(new String[]{
-                "127.0.0.1",
-                "127.0.1.1",
-                "0.0.0.0"
-            });
+	    Arrays.asList(new String[]{
+		"127.0.0.1",
+		"127.0.1.1",
+		"0.0.0.0"
+	    });
 
     private NetUtilities() {
     }
 
     public static final InetAddress guessLocalInetAddress() throws NetUtilitiesException {
-        try {
-            Enumeration<NetworkInterface> nics =
-                    NetworkInterface.getNetworkInterfaces();
-            InetAddress addr = null;
-            while (nics.hasMoreElements()) {
-                NetworkInterface current = nics.nextElement();
-                if (current.getHardwareAddress() == null) {
-                    continue;
-                } else {
-                    Enumeration<InetAddress> addresses =
-                            current.getInetAddresses();
-                    while (addresses.hasMoreElements()) {
-                        InetAddress currentAddr =
-                                addresses.nextElement();
-                        if (currentAddr.getClass() !=
-                                Inet4Address.class) {
-                            continue;
-                        }
-                        if (KNOWN_LOCAL_HOSTS.contains(
-                                currentAddr.toString())) {
-                            continue;
-                        } else {
-                            addr = currentAddr;
-                            break;
-                        }
-                    }
-                    if (addr != null) {
-                        break;
-                    }
-                }
-            }
-            if (addr != null) {
-                return addr;
-            } else {
-                throw new NetUtilitiesException("Unable to " +
-                        "guess external address...");
-            }
-        } catch (SocketException ex) {
-            throw new NetUtilitiesException(
-                    ex.getMessage(), ex);
-        }
+	try {
+	    Enumeration<NetworkInterface> nics =
+		    NetworkInterface.getNetworkInterfaces();
+	    InetAddress addr = null;
+	    while (nics.hasMoreElements()) {
+		NetworkInterface current = nics.nextElement();
+		if (current.getHardwareAddress() == null) {
+		    continue;
+		} else {
+		    Enumeration<InetAddress> addresses =
+			    current.getInetAddresses();
+		    while (addresses.hasMoreElements()) {
+			InetAddress currentAddr =
+				addresses.nextElement();
+			if (currentAddr.getClass()
+				!= Inet4Address.class) {
+			    continue;
+			}
+			if (KNOWN_LOCAL_HOSTS.contains(
+				currentAddr.toString())) {
+			    continue;
+			} else {
+			    addr = currentAddr;
+			    break;
+			}
+		    }
+		    if (addr != null) {
+			break;
+		    }
+		}
+	    }
+	    if (addr != null) {
+		return addr;
+	    } else {
+		throw new NetUtilitiesException("Unable to "
+			+ "guess external address...");
+	    }
+	} catch (SocketException ex) {
+	    throw new NetUtilitiesException(
+		    ex.getMessage(), ex);
+	}
     }
 }
