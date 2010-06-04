945fb56c5dc2277b7bd6b9dacdab42f134bedc21
Justin Smith <magno32@gmail.com>
2010-06-04 04:14:18 +0000

    Working on the site. Added the apache license, need to change this at SF.

--- jbeacon/src/main/java/com/summit/jbeacon/util/AutomaticallyRetryingOperation.java
+++ jbeacon/src/main/java/com/summit/jbeacon/util/AutomaticallyRetryingOperation.java
@@ -1,15 +1,18 @@
 /*
- * Copyright 2010 Summit Management Systems, Inc.
- * All rights reserved.
+ * Copyright 2009 Summit Management Systems, Inc.
  *
- * MODIFICATION/REDISTRIBUTION OF ANY PART OF THIS SOFTWARE (WHICH
- * INCLUDES, BUT IS NOT LIMITED TO, SOURCE CODE AND/OR COMPILED
- * BINARIES) IS STRICTLY PROHIBITED, WITH THE FOLLOWING EXCEPTION:
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *     API documentation may be freely redistributed, without
- *     modification, provided that no fees are charged.
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-
 package com.summit.jbeacon.util;
 
 import java.text.MessageFormat;
@@ -21,32 +24,36 @@ import org.apache.commons.logging.LogFactory;
  * @author <a href="mailto:steven.swor@summitsystemsinc.com" title="steven.swor@summitsystemsinc.com">Steven Swor</a>
  */
 public abstract class AutomaticallyRetryingOperation<T extends Object> {
-	private final int maxRetries;
-	private static final Log logger = LogFactory.getLog(AutomaticallyRetryingOperation.class);
-	public AutomaticallyRetryingOperation(final int maxAttempts) {
-		super();
-		this.maxRetries = maxAttempts;
-	}
-	protected abstract T operationToRetry() throws Exception;
-	public T execute() throws Exception {
-		T results = null;
-		for (int i=1;i<=this.maxRetries;i++) {
-			try {
-				results = operationToRetry();
-				if (i>1 && logger.isWarnEnabled()) {
-					logger.warn(MessageFormat.format("Operation succeeded on the {0, number,integer}{0, choice, 1#st | 2#nd | 3#rd | 3<th} attempt.", i));
-				}
-				break;
-			}catch(Exception ex) {
-				if (i<this.maxRetries) {
-					if (logger.isWarnEnabled()) {
-						logger.warn(MessageFormat.format("Operation failed for the {0, number,integer}{0, choice, 1#st | 2#nd | 3#rd | 3<th} time.  Retrying.", i));
-					}
-				} else {
-					throw ex;
-				}
-			}
+
+    private final int maxRetries;
+    private static final Log logger = LogFactory.getLog(AutomaticallyRetryingOperation.class);
+
+    public AutomaticallyRetryingOperation(final int maxAttempts) {
+	super();
+	this.maxRetries = maxAttempts;
+    }
+
+    protected abstract T operationToRetry() throws Exception;
+
+    public T execute() throws Exception {
+	T results = null;
+	for (int i = 1; i <= this.maxRetries; i++) {
+	    try {
+		results = operationToRetry();
+		if (i > 1 && logger.isWarnEnabled()) {
+		    logger.warn(MessageFormat.format("Operation succeeded on the {0, number,integer}{0, choice, 1#st | 2#nd | 3#rd | 3<th} attempt.", i));
 		}
-		return results;
+		break;
+	    } catch (Exception ex) {
+		if (i < this.maxRetries) {
+		    if (logger.isWarnEnabled()) {
+			logger.warn(MessageFormat.format("Operation failed for the {0, number,integer}{0, choice, 1#st | 2#nd | 3#rd | 3<th} time.  Retrying.", i));
+		    }
+		} else {
+		    throw ex;
+		}
+	    }
 	}
+	return results;
+    }
 }
